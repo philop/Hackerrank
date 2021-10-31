b4b70e684a3f94e2776d66b034182215e71217c4
Filip <pomarka.f@gmail.com>
2021-10-31 22:58:40 +0100

    so so version

--- src/com/triplets/Triplets.java
+++ src/com/triplets/Triplets.java
@@ -10,18 +10,39 @@ import java.util.stream.IntStream;
 public class Triplets {
 
     static long countTriplets(List<Long> arr, long r) {
-        Map<Long, Set<Integer>> indexCountingMap = IntStream.range(0, arr.size()).boxed().collect(Collectors.groupingBy(index -> arr.get(index), Collectors.toCollection(TreeSet::new)));
-        Map<Long, Long> countingMap = indexCountingMap.entrySet().stream().collect(Collectors.toMap(Map.Entry<Long, Set<Integer>>::getKey, entry->Long.valueOf(entry.getValue().size())));
+        Map<Long, TreeSet<Integer>> indexCountingMap = IntStream.range(0, arr.size()).boxed()
+                .collect(Collectors.groupingBy(arr::get, Collectors.toCollection(()->new TreeSet<>(Collections.reverseOrder()))));
+        Map<Long, Long> countingMap = indexCountingMap.entrySet().stream().collect(Collectors.toMap(Map.Entry<Long, TreeSet<Integer>>::getKey, entry->Long.valueOf(entry.getValue().size())));
         //Map<Long, Long> countingMap = arr.stream().collect(Collectors.groupingBy(el -> el, Collectors.counting()));
         return countingMap.entrySet().stream().map((el) -> {
             Long counterMid = countingMap.get(el.getKey() * r);
             Long counterEnd = countingMap.get(el.getKey() * r * r);
             if (counterMid != null && counterEnd != null && r != 1)
-                return (el.getValue()) * counterMid * counterEnd * 1l;
-            else if (counterMid != null && counterEnd != null && r == 1)
-                return (el.getValue()) * (counterMid - 1) * (counterEnd - 2)/6l;
-            else return 0l;
-        }).reduce(0l, (el, accumulator) -> accumulator + el);
+                //return el.getValue() * counterMid * counterEnd;
+                return countPermutations(r, indexCountingMap, el);
+            else if (counterMid != null && counterEnd != null)
+                return el.getValue() * (counterMid - 1) * (counterEnd - 2)/ 6L;
+            else return 0L;
+        }).reduce(0L, Long::sum);
+    }
+
+    private static Long countPermutations(long r, Map<Long, TreeSet<Integer>> indexCountingMap, Map.Entry<Long, Long> el) {
+        var indexSetStarts = indexCountingMap.get(el.getKey());
+        var indexSetMids = indexCountingMap.get(el.getKey() * r);
+        var indexSetEnds = indexCountingMap.get(el.getKey() * r * r);
+
+        Long reducedMids = indexSetEnds.stream().map(end -> {
+            if(indexSetMids.higher(end) != null)
+                return indexSetMids.tailSet(indexSetMids.higher(end)).size() *1L;
+            else return 1L;
+        }).reduce(1L, (a, b) -> a * b);
+        Long reducedStarts = indexSetMids.stream().map(end -> {
+            if(indexSetStarts.higher(end) != null)
+                return indexSetStarts.tailSet(indexSetStarts.higher(end)).size() * 1L;
+            else return 1L;
+        }).reduce(1L, (a, b) -> a * b);
+
+        return (long) indexSetEnds.size() * reducedMids * reducedStarts;
     }
 
     private String readFile(String name){
@@ -44,12 +65,12 @@ public class Triplets {
     public static void main(String args[]) {
 
         Triplets t = new Triplets();
+        testCase("1 2 1 2 4", 2, 3);
         testCase("1 2 2 4", 2, 2);
         testCase("1 3 9 9 27 81", 3, 6);
         testCase("1 5 5 25 125", 5, 4);
-        testCase(t.readFile( "input.txt"), 3, 2325652489l);
         testCase("1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1", 1, 161700);
-
+        //testCase(t.readFile( "input.txt"), 3, 2325652489l);
     }
 
     private static void testCase(String testCase, int r, long result) {
